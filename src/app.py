92e59db42ba6d69bfdbc916f4bf633ca2d828dac
J <my@email.com>
2017-02-13 09:49:09 +0000

    assignment 5

--- src/app.py
+++ src/app.py
@@ -34,6 +34,85 @@ def login():
     else:
          return 'Wrong access'
 
+#@app.route('rest/lost_key',methods=('POST',))
+#def lost_key():
+#    if request.method == 'POST':
+        
+
+
+
+@app.route('/rest/list_products',methods=('POST',))
+def list_products():
+    if request.method == 'POST' and 'arguments' in request.form:
+        req = json.loads(request.form['arguments'])
+    else:
+        redirect('rest') 
+
+    if len(req['compartments'])==0:
+        print("have not compartment")
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',')from products p 
+left join security_tags t on p.product_pk=t.product_fk 
+left join sec_compartments c on t.compartment_fk=c.compartment_pk 
+left join sec_levels l on t.level_fk=l.level_pk"""
+        if req['vendor']=='' and req['description']=='':
+            SQLstart += " group by vendor,description"
+            cursor.execute(SQLstart)
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s and vendor ilike %s group by vendor,description"
+                cursor.execute(SQLstart,(req['description'],req['vendor']))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " where description ilike %s group by vendor,description"
+                cursor.execute(SQLstart,(req['description'],))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " where vendor ilike %s group by vendor,description"
+                cursor.execute(SQLstart,(req['vendor'],))
+    else:
+        print("have compartment %s"%len(req['compartments']))
+        SQLstart = """select vendor,description,string_agg(c.abbrv||':'||l.abbrv,',') from security_tags t
+left join sec_compartments c on t.compartment_fk=c.compartment_pk
+left join sec_levels l on t.level_fk=l.level_pk
+left join products p on t.product_fk=p.product_pk
+where product_fk is not NULL and c.abbrv||':'||l.abbrv = ANY(%s)"""
+        if req['vendor']=='' and req['description']=='':
+            SQLstart += " group by vendor,description,product_fk having count(*)=%s"
+            cursor.execute(SQLstart,(req['compartments'],len(req['compartments'])))
+        else:
+            if not req['vendor']=='' and not req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cursor.execute(SQLstart,(req['compartments'],req['description'],req['vendor'],len(req['compartments'])))
+            elif req['vendor']=='':
+                req['description']="%"+req['description']+"%"
+                SQLstart += " and description ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cursor.execute(SQLstart,(req['compartments'],req['description'],len(req['compartments'])))
+            elif req['description']=='':
+                req['vendor']="%"+req['vendor']+"%"
+                SQLstart += " and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+                cursor.execute(SQLstart,(req['compartments'],req['vendor'],len(req['compartments'])))
+    dbres = cursor.fetchall()
+    listing = list()
+    for row in dbres:
+        e = dict()
+        e['vendor'] = row[0]
+        e['description'] = row[1]
+        if row[2] is None:
+            e['compartments'] = list()
+        else:
+            e['compartments'] = row[2].split(',')
+        listing.append(e)
+    dat = dict()
+    dat['timestamp'] = req['timestamp']
+    dat['listing'] = listing
+    data = json.dumps(dat)
+
+    conn.close()
+    return data
 
 @app.route('/rest/suspend_user',methods=('POST',))
 def suspend_user():
@@ -54,7 +133,7 @@ def activate_user():
 
     dat = dict()
     dat['timestamp'] = req['timestamp']
-#   dat['username'] = req['username']
+#    dat['username'] = req['username']
     dat['result'] = 'OK'
     data = json.dumps(dat)
 
@@ -64,6 +143,111 @@ def activate_user():
     conn.commit()
     return data
 
+
+
+@app.route('/rest/add_products',methods=('POST',))
+def add_products():
+    if request.method == 'POST' and 'arguments' in request.form:
+        req = json.loads(request.form['arguments'])
+
+    if len(sys.argv)<6 :
+        return 
+
+    # Prep the arguments blob
+    args = dict()
+    args['timestamp'] = req['timestamp']
+
+    prod = dict()
+    prod['vendor'] = sys.argv[2]
+    prod['description'] = sys.argv[3]
+    prod['alt_description'] = sys.argv[4]
+    prod['compartments'] = sys.argv[5]
+    prod_list = list()
+    pro_list.append(prod)
+    args['new_products'] = prod_list
+
+    # Setup the data to send
+    sargs = dict()
+    sargs['arguments']=json.dumps(args)
+    sargs['signature']=''
+    data = urlencode(sargs)
+
+
+    return data
+
+
+#@app.route('/rest/add_products',methods=('POST',))
+#def add_products():
+#    if request.method == 'POST' and 'arguments' in request.form:
+#        req = json.loads(request.form['arguments'])
+
+#    if len(req['compartments'])==0:
+#        print("have not compartment")
+#        SQLstart = """select vendor, description,alt_description,string_agg(c.abbrv||':'||l.abbrv,',') from products p
+#left join security_tags t on p.product_pk=t.product_fk
+#left join sec_compartments c on t.compartment_fk=c.compartment_pk
+#left join sec_levels l on t.level_fk=l.level_pk"""
+#        if req['vendor']=='' and req['description']=='' and req['alt_description']=='':
+#            SQLstart += "group by vendor,description,alt_description"
+#            cursor.execute(SQLstart)
+#        else:
+#            if not req['vendor']=='' and not req['description']=='':
+#                req['vendor']="%"+req['vendor']+"%"
+#                req['description']="%"+req['description']+"%"
+#                SQLstart += " where description ilike %s and vendor ilike %s group by vendor,description"
+#                cursor.execute(SQLstart,(req['description'],req['vendor']))
+#            elif req['vendor'] =='':
+#                req['description']="%"+req['description']+"%"
+#                SQLstart += " where description ilike %s group by vendor,description"
+#                cur.execute(SQLstart,(req['description'],))
+#            elif req['description']=='':
+#                req['vendor']="%"+req['vendor']+"%"
+#                SQLstart += " where vendor ilike %s group by vendor,description"
+#                cursor.execute(SQLstart,(req['vendor'],))
+#    else:
+#        print("have compartment %s"%len(req['compartments']))
+#        SQLstart = """select vendor,description,alt_description,string_agg(c.abbrv||':'||l.abbrv,',')
+#        from security_tags t
+#left join sec_compartments c on t.compartment_fk=c.compartment_pk
+#left join sec_levels l on t.level_fk=l.level_pk
+#left join products p on t.product_fk=p.product_pk
+#where product_fk is not NULL and c.abbrv||':'||l.abbrv = ANY(%s)"""
+#        if req['vendor']=='' and req['description']=='':
+#            SQLstart += " group by vendor,description,product_fk having count(*)=%s"
+#            cursor.execute(SQLstart,(req['compartments'],len(req['compartments'])))
+#        else:
+#            if not req['vendor']=='' and not req['description']=='':
+#                req['vendor']="%"+req['vendor']+"%"
+#                req['description']="%"+req['description']+"%"
+#                SQLstart += " and description ilike %s and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+#                cursor.execute(SQLstart,(req['compartments'],req['description'],req['vendor'],len(req['compartments'])))
+#            elif req['vendor']=='':
+#                req['description']="%"+req['description']+"%"
+#                SQLstart += " and description ilike %s group by vendor,description,product_fk having count(*)=%s"
+#                cursor.execute(SQLstart,(req['compartments'],req['description'],len(req['compartments'])))
+#            elif req['description']=='':
+#                req['vendor']="%"+req['vendor']+"%"
+#                SQLstart += " and vendor ilike %s group by vendor,description,product_fk having count(*)=%s"
+#                cursor.execute(SQLstart,(req['compartments'],req['vendor'],len(req['compartments'])))
+
+#    dbres = cursor.fetchall()
+#    listing = list()
+#    for row in dbres:
+#        e = dict()
+#        e['vendor'] = row[0]
+#        e['description'] = row[1]
+#        e['alt_description'] = row[2]
+#        e['compartments'] = row[3]
+#        listing.append(e)
+
+    #prepare the response
+#    dat = dict()
+#    dat['timestamp'] = req['timestamp']
+#    dat['result'] = 'OK'
+#    data = json.dumps(dat)
+    
+#    conn.close()
+#    return data
 
 #report filter screen 
 @app.route('/report_filter_screen/')
